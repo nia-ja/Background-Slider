529d29a720d23dd9d3ba4ec2af60cdf6eaff621c
Kasia <kasia.bondarava@gmail.com>
2023-02-03 10:59:53 -0500

    Changing slides functionality

--- script.js
+++ script.js
@@ -1,1 +1,42 @@
-window.onload(console.log("Initializing"));
+const body = document.body;
+const slides = document.querySelectorAll(".slide");
+const leftBtn = document.getElementById("left");
+const rightBtn = document.getElementById("right");
+
+// Event listeners
+leftBtn.addEventListener("click", leftClick);
+rightBtn.addEventListener("click", rightClick);
+
+let activeSlide = 0;
+
+// Handles background image
+function setBgToBody() {
+    body.style.backgroundImage = slides[activeSlide].style.backgroundImage;
+}
+
+// Handles visability
+function setActiveSlide() {
+    slides.forEach(slide => slide.classList.remove("active"));
+    slides[activeSlide].classList.add("active");
+}
+
+function leftClick() {
+    activeSlide--;
+    if (activeSlide < 0) {
+        activeSlide = slides.length - 1;
+    }
+    setBgToBody();
+    setActiveSlide(); 
+}
+
+function rightClick() {
+    activeSlide++;
+    if(activeSlide > slides.length - 1) {
+        activeSlide = 0 ;
+    }
+    setBgToBody();
+    setActiveSlide();
+}
+
+// load background image from the start
+setBgToBody();
